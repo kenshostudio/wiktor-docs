ccceb2130e3f47a9de27afbb3657f17c79072617
gabrieluhlir <69206870+gauel@users.noreply.github.com>
2022-03-29 03:41:53 +0200

    feat(Config): Add vuepress configuration

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,88 @@
+module.exports = {
+	theme: '@vuepress/theme-default',
+	themeConfig: {
+		logo: '/images/wiktor/wiktor-docs-logo.svg',
+		logoDark: '/images/wiktor/wiktor-docs-logo-dark.svg',
+
+		navbar: [
+
+			{
+				text: 'For Users',
+				link: '/user/',
+			},
+			{
+				text: 'For Business',
+				link: '/business/'
+			},
+			{
+				text: 'Wiktor links',
+				children: [
+					{
+						text: 'Forum',
+						link: 'https://forum.wiktor.app'
+					},
+					{
+						text: 'App',
+						link: 'https://wiktor.app'
+					},
+					{
+						text: 'Website',
+						link: 'https://thewiktor.com'
+					},
+				]
+			},
+		],
+		sidebar: {
+			'/': [
+
+			],
+			'/user/': [
+				{
+					text: 'For users',
+					children: [
+						'/user/README.md',
+						'/user/getting-started.md'
+					],
+				},
+			],
+			'/business/': [
+				{
+					text: 'For business',
+					children: ['/business/cli.md', '/business/config.md'],
+				},
+			],
+		},
+	},
+	locales: {
+		'/': {
+			lang: 'en-US',
+			description: 'Documentation',
+			selectLanguageName: 'English',
+			selectLanguageText: 'Language',
+		},
+		'/cz/': {
+			selectLanguageName: 'Čeština',
+			lang: 'Čeština',
+			description: 'Dokumentace',
+			selectLanguageText: 'Jazyk',
+		},
+	},
+	postcss: {
+		plugins: [require('tailwindcss')('./tailwind.js'), require('autoprefixer')],
+	},
+	plugins: [
+		[
+			'@vuepress/plugin-search',
+			{
+				locales: {
+					'/': {
+						placeholder: 'Search',
+					},
+					'/zh/': {
+						placeholder: '搜索',
+					},
+				},
+			},
+		],
+	]
+}
